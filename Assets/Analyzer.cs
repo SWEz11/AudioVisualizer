93d0e2bfa69e43b7d8f1792ce80a142258e65f05
swez <137026304+SWEz11@users.noreply.github.com>
2024-11-18 17:42:06 +0200

    ok

--- Assets/Analyzer.cs
+++ Assets/Analyzer.cs
@@ -8,19 +8,34 @@ public class Analyzer : MonoBehaviour
     // Start is called before the first frame update
     public float startTime;
     AudioSource source;
+    public bool isPaused;
     void Start()
     {
         source = GetComponent<AudioSource>();
         source.time = startTime;
+        isPaused = false;
     }
 
-    public void Pause()
-    {   
-        source.Pause();
+    public void PauseResume()
+    {
+        if (source.isPlaying)
+        {
+            source.Pause();
+        }
+        else
+        {
+            source.UnPause();
+        }
     }
 
     public void SetTime(float time)
     {
         source.time = time;
+    }
+
+
+    public void Volume(float volume)
+    {
+        source.volume = volume;
     }
 }
